f34055b6e21a4d63d5e0c60fb914ca1ef40c00c3
Mohammed Binsabbar <ibinsabbar@gmail.com>
2015-02-17 12:30:03 +0000

    Implement GoogleAPIAuthenticationManager
    This file includes two classes. The first class is responsible for
    authorisation/authentication for installed application account. This
    class can accept authorisation code as user input.
    
    The 2nd class is responsible authorisation/authentication for
    service account. Currently, the class is able to authenticates itself and list files that have been shared with it. 
    NOTE: Accessing user’s account is not done yet for this
    type of client accounts. (Domain-Wide Delegation authorisation is needed https://developers.google.com/drive/web/delegation)

---
+++ lib/admix/google_drive_client_manager.rb
@@ -0,0 +1,215 @@
+require 'google/api_client'
+require 'google_drive'
+require 'json'
+
+module GoogleDriveApiHelper
+
+  CLIENT_SCOPE = 'https://www.googleapis.com/auth/drive'
+  AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
+  TOKEN_URI =  "https://accounts.google.com/o/oauth2/token"
+
+  class AuthenticationManagerForInstalledAppClient
+
+    attr_reader :access_token
+
+    def initialize(client_id, client_secret, authorization_file, user_email)
+      @authorization_file = authorization_file
+      client = Google::APIClient.new(:application_name =>"Admix", :application_version=>"0.3")
+      @auth = client.authorization
+      @auth.client_id = client_id
+      @auth.client_secret = client_secret
+      @auth.scope = CLIENT_SCOPE
+      @auth.username = user_email
+      @auth.redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+    end
+
+    def access_token
+      if (not get_stored_credentials) || @auth.refresh_token.nil?
+        return nil unless perform_authentication
+      end
+
+      if @auth.access_token.nil? || has_token_expired
+        refresh_token
+      end
+
+      @access_token = @auth.access_token
+    end
+
+
+    def perform_authentication
+      @auth.grant_type = ''
+      authorization_retry = 0
+      begin
+        print("1. Open this page:\n%s\n\n" % @auth.authorization_uri)
+        print("\n2. Enter the authorization code shown in the page: ")
+        @auth.code = gets.chomp
+        @auth.fetch_access_token!
+        save_authorization_file
+        true
+      rescue Signet::AuthorizationError
+        print("\n Authorisation failed.\n")
+        @auth.access_token = nil
+        false
+      rescue ArgumentError
+        print("\n Authorisation failed. Try again\n")
+        if authorization_retry < 2
+          authorization_retry += 1
+          retry
+        end
+        print("\n Authorisation failed 3 times.\n")
+        @auth.access_token = nil
+        false
+      end
+    end
+
+    private
+
+    def has_token_expired
+      Time.now > @auth.expires_at
+    end
+
+    def refresh_token
+      begin
+        @auth.grant_type = "refresh_token"
+        @auth.fetch_access_token!
+        @auth.expires_at = Time.now + @auth.expires_in
+        save_authorization_file
+        true
+      rescue Signet::AuthorizationError
+        perform_authentication
+      end
+    end
+
+    def save_authorization_file()
+      token_hash = {:access_token => @auth.access_token,
+                    :refresh_token => @auth.refresh_token,
+                    :expires_in => @auth.expires_in,
+                    :expires_at => @auth.expires_at.to_s,
+                    :user_email => @auth.username}
+
+      File.open(@authorization_file, 'w+') do |f|
+        f.write(JSON.pretty_generate(token_hash))
+      end
+    end
+
+    def get_stored_credentials()
+      if File.exists?(@authorization_file)
+        file = File.read(@authorization_file)
+        token_hash = JSON.parse(file)
+        @auth.access_token = token_hash["access_token"]
+        @auth.refresh_token = token_hash["refresh_token"]
+        @auth.expires_in = token_hash["expires_in"]
+        @auth.expires_at = Time.parse(token_hash["expires_at"])
+        if @auth.username == token_hash['user_email']
+          @auth.username = token_hash['user_email']
+          return true
+        end
+      end
+      return false
+    end
+  end
+
+  class GoogleDriveServiceAccountClient
+
+    SERVICE_ACCOUNT_PKCS12_FILE = File.expand_path('../../assets/key.p12', __FILE__)
+
+    CLIENT_EMAIL = "157478480211-bev4be6lh4762k893ir60uov75lqi8hn@developer.gserviceaccount.com"
+    CLIENT_SECRET = "notasecret"
+
+    def initialize(app_name, app_version)
+      @client = Google::APIClient.new(:application_name => app_name,
+                                      :application_version => app_version)
+      @drive = @client.discovered_api('drive', 'v2')
+    end
+
+    def perform_authentication (user_email=nil)
+      pkcs12_key = Google::APIClient::KeyUtils.load_from_pkcs12(SERVICE_ACCOUNT_PKCS12_FILE,
+                                                                CLIENT_SECRET)
+
+      if not user_email.nil?
+        @client.authorization = Signet::OAuth2::Client.new(
+            :token_credential_uri => TOKEN_URI,
+            :audience => TOKEN_URI,
+            :scope => CLIENT_SCOPE,
+            :issuer => CLIENT_EMAIL,
+            :sub => user_email,
+            :signing_key => pkcs12_key)
+      else
+        @client.authorization = Signet::OAuth2::Client.new(
+            :token_credential_uri => TOKEN_URI,
+            :audience => TOKEN_URI,
+            :scope => CLIENT_SCOPE,
+            :issuer => CLIENT_EMAIL,
+            :signing_key => pkcs12_key)
+      end
+
+      begin
+        @client.authorization.fetch_access_token!
+      rescue Signet::AuthorizationError
+        return false
+      end
+      return true
+    end
+
+    def list_all_files_for_the_service_account
+      result = Array.new
+      page_token = nil
+      begin
+        parameters = {}
+        if page_token.to_s != ''
+          parameters['pageToken'] = page_token
+        end
+        api_result = @client.execute(
+            :api_method => @drive.files.list,
+            :parameters => parameters)
+        if api_result.status == 200
+          files = api_result.data
+          result.concat(files.items)
+          page_token = files.next_page_token
+        else
+          puts "An error occurred: #{result.data['error']['message']}"
+          page_token = nil
+        end
+      end while page_token.to_s != ''
+      result
+    end
+
+    def download_file(file)
+      if file.download_url
+        result = @client.execute(:uri => file.download_url)
+        if result.status == 200
+          return result.body
+        else
+          puts "An error occurred: #{result.data['error']['message']}"
+          return nil
+        end
+      else
+        return nil
+      end
+    end
+
+    def update_file(id, title, mime_type, file_path)
+      result = @client.execute(
+          :api_method => @drive.files.get,
+          :parameters => { 'fileId' => id})
+      if result.status == 200
+        file = result.data
+        file.title = title
+        file.mime_type = mime_type
+        media = Google::APIClient::UploadIO.new(file_path, file.mime_type)
+        result = @client.execute(
+            :api_method => @drive.files.update,
+            :body_object => file,
+            :media => media,
+            :parameters => { 'fileId' => id,
+                             'uploadType' => 'multipart',
+                             'newRevision' => true,
+                             'alt' => 'json' })
+        if result.status == 200
+          return result.data
+        end
+      end
+      return nil
+    end
+  end
+end
---
+++ spec/google_driver_spec.rb
@@ -0,0 +1,169 @@
+require 'rspec'
+require 'google/api_client'
+
+require_relative '../spec/spec_helper'
+require_relative '../lib/admix/google_drive_client_manager'
+
+include GoogleDriveApiHelper
+RSpec.describe AuthenticationManagerForInstalledAppClient do
+
+  before(:all) do
+    @client_id ="157478480211-fqa6jd2lucmssqcjvpra07ih39u93etu.apps.googleusercontent.com"
+    @client_secret = "oSAoSvc0dxg4H-VTI3OBXLyV"
+    @user_email = "mbinsabb@thoughtworks.com"
+  end
+
+  def create_file(file, access_token = "random", refresh_token="random",
+                  expires_at=(Time.now + 3600), user_email=@user_email)
+    token_hash = {:access_token => access_token,
+                  :refresh_token => refresh_token,
+                  :expires_in => 3600,
+                  :expires_at => expires_at.to_s,
+                  :user_email => user_email
+    }
+    File.open(file, 'w+') do |f|
+      f.write(JSON.pretty_generate(token_hash))
+    end
+  end
+
+  describe "Prompts User for Drive Access Authorization to Client" do
+
+    before(:each) do
+      @number_of_prompt_calls = 3
+      @promption_message = "\n2. Enter the authorization code shown in the page: "
+      allow_any_instance_of(AuthenticationManagerForInstalledAppClient).to receive(:gets).and_return("!212")
+      allow_any_instance_of(AuthenticationManagerForInstalledAppClient).to receive(:print)
+    end
+
+    it "prompts user when file is not found" do
+      manager = AuthenticationManagerForInstalledAppClient.new(@client_id, @client_secret,
+                                                               'does_not_exist.json', @user_email)
+      manager.access_token
+
+      expect(manager).to(have_received(:gets).exactly(@number_of_prompt_calls))
+      expect(manager).to(have_received(:print).with(@promption_message).exactly(@number_of_prompt_calls))
+    end
+
+    it "prompts user when user_email in the file is different from the given user_email" do
+      file = File.expand_path("../assets/auth_data.json", __FILE__)
+      create_file(file)
+      manager = AuthenticationManagerForInstalledAppClient.new(@client_id, @client_secret,
+                                                               file, "newusername@gmail.com")
+
+      manager.access_token
+
+      expect(manager).to(have_received(:gets).exactly(@number_of_prompt_calls))
+      expect(manager).to(have_received(:print).with(@promption_message).exactly(@number_of_prompt_calls))
+    end
+
+    it "prompts user when refresh_token is nil" do
+      file = File.expand_path("../assets/auth_data.json", __FILE__)
+      create_file(file, "random access token", nil)
+      manager = AuthenticationManagerForInstalledAppClient.new(@client_id, @client_secret,
+                                                               file, @user_email)
+
+      manager.access_token
+
+      expect(manager).to have_received(:gets).exactly(@number_of_prompt_calls)
+      expect(manager).to have_received(:print).with(@promption_message).exactly(@number_of_prompt_calls)
+    end
+
+    it "prompts user when AuthorizationError is raised (refresh_token is invalid)" do
+      file = File.expand_path("../assets/auth_data.json", __FILE__)
+      create_file(file, nil, "random refresh token that is invalid")
+      manager = AuthenticationManagerForInstalledAppClient.new(@client_id, @client_secret, file,
+                                                               @user_email)
+
+      manager.access_token
+
+      expect(manager).to have_received(:gets).exactly(@number_of_prompt_calls)
+      expect(manager).to have_received(:print).with(@promption_message).exactly(@number_of_prompt_calls)
+    end
+  end
+
+  describe "Return an access token" do
+
+    it "returns an access token when it is found and has not expired" do
+      file = File.expand_path("../assets/auth_data.json", __FILE__)
+      create_file(file, "ya29.HAGs3ezBdmXFRcoceYVoKhQ0aLnh27Mo6OJVaxyiidtYtOr9yCzGapY60CUWT72")
+      manager = AuthenticationManagerForInstalledAppClient.new(@client_id, @client_secret, file,@user_email)
+
+      expect(manager.access_token).to eq "ya29.HAGs3ezBdmXFRcoceYVoKhQ0aLnh27Mo6OJVaxyiidtYtOr9yCzGapY60CUWT72"
+    end
+
+    it "generates a new access token and returns it when current token has expired" do
+      file = File.expand_path("../assets/auth_data_expired.json", __FILE__)
+      create_file(file, nil, "1/bJHUo7RGfoXXgLli5XG9ytnWZUmN0DzM2aJOw_aeVFM", "2015-02-15 09:23:22 +0000")
+      manager = AuthenticationManagerForInstalledAppClient.new(@client_id, @client_secret, file, @user_email)
+
+      expect(manager.access_token).to_not be_nil
+    end
+  end
+end
+
+RSpec.describe GoogleDriveServiceAccountClient do
+
+  before(:all) do
+    @wrapper = GoogleDriveServiceAccountClient.new("app_name", "app_version")
+  end
+
+  describe "Authenticate client" do
+    it "authenticate the service client, if no email is given" do
+      expect(@wrapper.perform_authentication).to eq true
+    end
+
+    it "returns false when trying to generate token for a user, if the user has not authorized the client" do
+      expect(@wrapper.perform_authentication "fake@email.com").to eq false
+    end
+
+    it "returns true when user_email has given authorization to the service account" do
+      user_email = 'mbinsabb@thoughtworks.com'
+      expect(@wrapper.perform_authentication user_email).to eq true
+    end
+  end
+
+  describe "Dealing with Files" do
+    before(:all) do
+      @wrapper.perform_authentication
+    end
+
+    it "returns a list of all files, which includes \"How to get started with Drive\", at the account service drive" do
+      files = @wrapper.list_all_files_for_the_service_account
+
+      expect(files.find {|file| file.title == 'How to get started with Drive'}).to_not be_nil
+    end
+
+    it "downloads \"How to get started with Drive\" PDF file given its file ID" do
+      files = @wrapper.list_all_files_for_the_service_account
+      file = files.find {|file| file.title == 'How to get started with Drive'}
+
+      expect(@wrapper.download_file file).to_not be_nil
+    end
+
+    it "Updates an existing file" do
+      files = @wrapper.list_all_files_for_the_service_account
+      file = files.find {|file| file.title == 'How to get started with Drive'}
+      path_to_file = File.expand_path("../assets/How to get started with Drive.pdf",__FILE__)
+
+      result = @wrapper.update_file(file.id, file.title, 'application/pdf', path_to_file)
+
+      expect(result).to be_a Google::APIClient::Schema::Drive::V2::File
+    end
+
+    it "Returns nil when trying to update a drive file that does not exist" do
+      path_to_file = File.expand_path("../assets/How to get started with Drive.pdf",__FILE__)
+
+      result = @wrapper.update_file("10827363", "How to get started with Drive", 'application/pdf', path_to_file)
+
+      expect(result).to be_nil
+    end
+
+    it "Returns nil when trying to update a drive file with a file that does not exist locally" do
+      path_to_file = File.expand_path("../assets/I do not exist.pdf",__FILE__)
+
+      result = @wrapper.update_file("10827363", "How to get started with Drive", 'application/pdf', path_to_file)
+
+      expect(result).to be_nil
+    end
+  end
+end
