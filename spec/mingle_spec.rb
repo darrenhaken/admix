b3b0212b252fc82674c60bc7552ff8efd001da96
Mohammed Binsabbar <ibinsabbar@gmail.com>
2015-02-17 12:42:49 +0000

    Use File.expand_path to generate a path to a file
    comment out a test that was not completed, and probably will be deleted
    among with its class

--- spec/mingle_spec.rb
+++ spec/mingle_spec.rb
@@ -26,10 +26,10 @@ module Admix
   end
 
   RSpec.describe MingleWallSnapshot do
+    let(:file){File.expand_path("../assets/mingle_story_response.xml",__FILE__)}
+    let(:mingle_stories_xml) {File.read(file)}
 
-	let(:mingle_stories_xml) {File.read('./assets/mingle_story_response.xml')}
-
-	subject(:mingle_story_wall_snapshot) {MingleWallSnapshot.new(mingle_stories_xml)}
+    subject(:mingle_story_wall_snapshot) {MingleWallSnapshot.new(mingle_stories_xml)}
 
     describe 'MingleWallSnapshot' do
       
@@ -47,19 +47,19 @@ module Admix
     end
   end
 
-  RSpec.describe CumulativeFlowDiagramSpreadsheet do
-
-    let(:cfd_spreadsheet) {CumulativeFlowDiagramSpreadsheet.new}
-
-    describe CumulativeFlowDiagramSpreadsheet do
-
-      it 'should authenticate with Google Drive' do
-        client = cfd_spreadsheet.build_client 'afahie@thoughtworks.com'
-        file = cfd_spreadsheet.get_file_metadata client, '0AqgwNq-F1tXfdEtFaEx4T0lmV3FrcmhjcEFBQzhxc1E'
-        #puts cfd_spreadsheet.download_file client, file
-      end
-
-    end
-  end
+  # RSpec.describe CumulativeFlowDiagramSpreadsheet do
+  #
+  #   let(:cfd_spreadsheet) {CumulativeFlowDiagramSpreadsheet.new}
+  #
+  #   describe CumulativeFlowDiagramSpreadsheet do
+  #
+  #     it 'should authenticate with Google Drive' do
+  #       client = cfd_spreadsheet.build_client 'afahie@thoughtworks.com'
+  #       file = cfd_spreadsheet.get_file_metadata client, '0AqgwNq-F1tXfdEtFaEx4T0lmV3FrcmhjcEFBQzhxc1E'
+  #       #puts cfd_spreadsheet.download_file client, file
+  #     end
+  #
+  #   end
+  # end
 
 end
