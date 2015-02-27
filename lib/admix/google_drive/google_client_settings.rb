3d671a98afca215171b1f73061e10f945ffe30d2
Mohammed Binsabbar <ibinsabbar@gmail.com>
2015-02-27 14:27:07 +0000

    create class for google settings

---
+++ lib/admix/google_drive/google_client_settings.rb
@@ -0,0 +1,11 @@
+class GoogleClientSettings
+
+  attr_reader client_id, client_secret, user_email
+
+  def initialize(client_id, client_secret, user_email)
+    @client_id = client_id
+    @client_secret = client_secret
+    @user_email = user_email
+  end
+
+end
