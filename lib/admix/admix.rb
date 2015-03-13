93f8ef92d99440814a1ba6049077ad96450a40cb
Mohammed Binsabbar <ibinsabbar@gmail.com>
2015-03-13 11:23:15 +0000

    Remove interactive shell with user

--- lib/admix/admix.rb
+++ lib/admix/admix.rb
@@ -2,14 +2,11 @@ require 'rest_client'
 
 require_relative 'version'
 
+require_relative '../../lib/admix/settings'
 require_relative '../../lib/admix/google_drive/google_controller'
 require_relative '../../lib/admix/google_drive/google_client_settings'
-
-require_relative '../../lib/admix/mingle/mingle_resource_loader'
-require_relative '../../lib/admix/mingle/mingle_wall_snapshot'
-require_relative '../../lib/admix/mingle/mql_parser'
-
-require_relative '../../lib/admix/settings'
+require_relative '../../lib/admix/mingle/mingle_controller'
+require_relative '../../lib/admix/mingle/mingle_settings'
 
 class AdmixApp
 
@@ -35,72 +32,16 @@ class AdmixApp
   end
 
   def print_statistics
-    print_card_statistics_for_type('Story')
-    print_card_statistics_for_type('Defect')
-    print_card_statistics_for_type('Power Ups')
-
-    print_card_statistics_for_status('Next')
-    print_card_statistics_for_status('A & D')
-    print_card_statistics_for_status('A & D done')
-    print_card_statistics_for_status('Dev')
-    print_card_statistics_for_status('Dev done')
-    print_card_statistics_for_status('QA')
-    print_card_statistics_for_status('QA done')
-  end
-
-  def print_card_statistics_for_type(type)
-    number = @mingle_wall.number_of_cards_of_type(type)
-    print("Number of cards with Type #{type}: #{number}\n")
-  end
-
-  def print_card_statistics_for_status(status)
-    number = @mingle_wall.number_of_cards_with_status(status)
-    print("Number of cards in Status #{status}: #{number} \n")
+    result = @mingle_controller.get_cards_statistics
+    result.each do |k, v|
+      print("\nNumber of cards in #{k} is #{v}")
+    end
   end
 
   private
-
-  def setup_mingle
-    print("\nEnter Mingle username \n=> ")
-    mingle_username = gets.chomp
-
-    print("\nEnter Mingle password \n=> ")
-    mingle_password = gets.chomp
-
-    print("\nEnter Mingle URL (without https/http and project name) \n=> ")
-    mingle_url = gets.chomp
-
-    print("\nEnter Mingle project name \n=> ")
-    mingle_project_name = gets.chomp
-
-    print("\nEnter path to mingle filter file \n=> ")
-    mingle_filter_file_path = gets.chomp
-
-    mingle_settings = MingleSettings.new(mingle_username, mingle_password, mingle_url, mingle_project_name)
-    create_mingle_with(mingle_settings, mingle_filter_file_path)
-  end
-
   def create_mingle_with(mingle_settings, filter_file_name)
-    @mingle_wrapper = MingleResourceLoader.new(mingle_settings.username, mingle_settings.password,
-                                               mingle_settings.url, RestClient)
     full_path_to_file = File.expand_path("../#{filter_file_name}", __FILE__)
-    @mql_wrapper = MQLParser.new(full_path_to_file, 'name, type, status')
-    @mingle_wrapper.load_cards_for_project?(mingle_settings.project_name, @mql_wrapper.parse)
-    @mingle_wall = MingleWallSnapshot.new(@mingle_wrapper.resource)
-  end
-
-  def perform_google_auth
-    print("Enter Google Client ID for installed Application\n=> ")
-    client_id = gets.chomp
-
-    print("\nEnter Google Client Secret for installed Application\n=> ")
-    client_secret = gets.chomp
-
-    print("\nEnter your email address (to access your google drive files)\n=> ")
-    user_email = gets.chomp
-
-    google_settings = GoogleClientSettings.new(client_id, client_secret, user_email)
-    create_manager_with(google_settings)
+    @mingle_controller = MingleController.new(mingle_settings, full_path_to_file)
   end
 
   def create_manager_with(google_settings)
