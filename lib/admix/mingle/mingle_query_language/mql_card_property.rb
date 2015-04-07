1bae544d63b0d1bce55ccb01b6192e3754995dd5
Mohammed Binsabbar <ibinsabbar@gmail.com>
2015-04-07 16:27:47 +0100

    create mql card property from MQLCardProperty

---
+++ lib/admix/mingle/mingle_query_language/mql_card_property.rb
@@ -0,0 +1,25 @@
+class MQLCardProperty
+
+  attr_reader :property
+
+  def initialize(property)
+    @property = property
+  end
+
+  def and(another_mql_property)
+    MQLCardProperty.new("#{property}, #{another_mql_property.property}")
+  end
+
+  def self.name()
+    MQLCardProperty.new('name')
+  end
+
+  def self.status()
+    MQLCardProperty.new('status')
+  end
+
+  def self.type()
+    MQLCardProperty.new('type')
+  end
+
+end
---
+++ spec/unit/admix/mingle/mingle_query_language/mql_card_property_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+
+require_relative '../../../../../lib/admix/mingle/mingle_query_language/mql_card_property'
+
+RSpec.describe MQLCardProperty do
+
+  it 'returns MQLCardProperty with property "name"' do
+    mql_card_property = MQLCardProperty.name
+
+    expect(mql_card_property).to be_a MQLCardProperty
+    expect(mql_card_property.property).to eq 'name'
+  end
+
+  it 'returns MQLCardProperty with property "status"' do
+    mql_card_property = MQLCardProperty.status
+
+    expect(mql_card_property).to be_a MQLCardProperty
+    expect(mql_card_property.property).to eq 'status'
+  end
+
+  it 'returns MQLCardProperty with property "type"' do
+    mql_card_property = MQLCardProperty.type
+
+    expect(mql_card_property).to be_a MQLCardProperty
+    expect(mql_card_property.property).to eq 'type'
+  end
+
+  it 'returns MQLCardProperty with property as "name, type"' do
+    mql_card_property = MQLCardProperty.name.and(MQLCardProperty.type)
+
+    expect(mql_card_property).to be_a MQLCardProperty
+    expect(mql_card_property.property).to eq 'name, type'
+  end
+
+  it 'returns MQLCardProperty with property as "name, status, type"' do
+    mql_card_property = MQLCardProperty.name.and(MQLCardProperty.status).and(MQLCardProperty.type)
+
+    expect(mql_card_property).to be_a MQLCardProperty
+    expect(mql_card_property.property).to eq 'name, status, type'
+  end
+end
